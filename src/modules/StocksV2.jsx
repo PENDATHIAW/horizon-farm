dc8a92b53c4208487cb36ba321506ea466a2185f
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-09 01:35:13 +0000

    Show editable stock status panel

--- src/modules/StocksV2.jsx
+++ src/modules/StocksV2.jsx
@@ -1,9 +1,11 @@
 import Stock from './Stocks.jsx';
 import StockFlowPanel from './StockFlowPanel.jsx';
+import StockStatusPanel from './StockStatusPanel.jsx';
 
 export default function StocksV2(props) {
   return (
     <div className="space-y-6">
+      <StockStatusPanel {...props} />
       <StockFlowPanel {...props} />
       <Stock {...props} />
     </div>
