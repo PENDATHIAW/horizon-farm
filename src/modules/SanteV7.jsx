7c808c1752290c3a1328df6db8f5e1097e3b90a9
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-10 23:34:34 +0000

    fix: keep health evolution charts

--- src/modules/SanteV7.jsx
+++ src/modules/SanteV7.jsx
@@ -1,5 +1,14 @@
 import SanteV6 from './SanteV6.jsx';
+import SanteEvolution from './SanteEvolution.jsx';
 
 export default function SanteV7(props) {
-  return <SanteV6 {...props} />;
+  return (
+    <div className="space-y-6">
+      <SanteV6 {...props} />
+      <SanteEvolution
+        rows={props.rows || []}
+        onNavigate={props.onNavigate}
+      />
+    </div>
+  );
 }
