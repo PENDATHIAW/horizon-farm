fb8ca6965d018ecf55ea28c2eec259fcbbda14f4
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-10 17:26:32 +0000

    feat: add direct charges section to animals module

--- src/modules/AnimauxV2.jsx
+++ src/modules/AnimauxV2.jsx
@@ -1,6 +1,7 @@
 import Animaux from './Animaux.jsx';
 import AnimalSlaughterStockBridge from './AnimalSlaughterStockBridge.jsx';
 import AnimauxEvolution from './AnimauxEvolution.jsx';
+import DirectChargesBridge from './DirectChargesBridge.jsx';
 
 export default function AnimauxV2(props) {
   return (
@@ -14,8 +15,22 @@ export default function AnimauxV2(props) {
         onCreateBusinessEvent={props.onCreateBusinessEvent}
         onRefreshBusinessEvents={props.onRefreshBusinessEvents}
       />
+      <DirectChargesBridge
+        title="Autres charges directes animaux"
+        subtitle="Ajoute les frais exceptionnels liés à un animal précis : transport, traitement spécial, abattage, emballage, etc."
+        targetType="animaux"
+        targets={props.rows || []}
+        businessEvents={props.businessEvents || []}
+        onCreateBusinessEvent={props.onCreateBusinessEvent}
+        onUpdateBusinessEvent={props.onUpdateBusinessEvent}
+        onDeleteBusinessEvent={props.onDeleteBusinessEvent}
+        onRefreshBusinessEvents={props.onRefreshBusinessEvents}
+      />
       <AnimauxEvolution
         rows={props.rows || []}
+        alimentationLogs={props.alimentationLogs || []}
+        vaccins={props.vaccins || []}
+        businessEvents={props.businessEvents || []}
         opportunities={props.opportunities || []}
         onNavigate={props.onNavigate}
       />
