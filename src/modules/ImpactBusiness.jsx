0fa432d35079a750026d8acdd5ea9a718abbd260
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-10 11:20:03 +0000

    Harden impact business module data links

--- src/modules/ImpactBusiness.jsx
+++ src/modules/ImpactBusiness.jsx
@@ -1,5 +1,19 @@
+import useCrudModule from '../hooks/useCrudModule';
 import ImpactBusinessStrategicV4 from './ImpactBusinessStrategicV4.jsx';
 
 export default function ImpactBusiness(props) {
-  return <ImpactBusinessStrategicV4 {...props} />;
+  const culturesCrud = useCrudModule('cultures');
+  const clientsCrud = useCrudModule('clients');
+  const fournisseursCrud = useCrudModule('fournisseurs');
+  const equipementsCrud = useCrudModule('equipements');
+
+  return (
+    <ImpactBusinessStrategicV4
+      {...props}
+      cultures={props.cultures?.length ? props.cultures : culturesCrud.rows}
+      clients={props.clients?.length ? props.clients : clientsCrud.rows}
+      fournisseurs={props.fournisseurs?.length ? props.fournisseurs : fournisseursCrud.rows}
+      equipements={props.equipements?.length ? props.equipements : equipementsCrud.rows}
+    />
+  );
 }
