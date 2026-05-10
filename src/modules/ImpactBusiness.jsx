fc6f30369e88e147b6c5c83c5edf9da4d5ea76c4
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-10 11:52:53 +0000

    Add farm value domains to impact module

--- src/modules/ImpactBusiness.jsx
+++ src/modules/ImpactBusiness.jsx
@@ -1,4 +1,5 @@
 import useCrudModule from '../hooks/useCrudModule';
+import ImpactFarmValueBridge from './ImpactFarmValueBridge.jsx';
 import ImpactBusinessStrategicV5 from './ImpactBusinessStrategicV5.jsx';
 
 export default function ImpactBusiness(props) {
@@ -7,13 +8,18 @@ export default function ImpactBusiness(props) {
   const fournisseursCrud = useCrudModule('fournisseurs');
   const equipementsCrud = useCrudModule('equipements');
 
+  const mergedProps = {
+    ...props,
+    cultures: props.cultures?.length ? props.cultures : culturesCrud.rows,
+    clients: props.clients?.length ? props.clients : clientsCrud.rows,
+    fournisseurs: props.fournisseurs?.length ? props.fournisseurs : fournisseursCrud.rows,
+    equipements: props.equipements?.length ? props.equipements : equipementsCrud.rows,
+  };
+
   return (
-    <ImpactBusinessStrategicV5
-      {...props}
-      cultures={props.cultures?.length ? props.cultures : culturesCrud.rows}
-      clients={props.clients?.length ? props.clients : clientsCrud.rows}
-      fournisseurs={props.fournisseurs?.length ? props.fournisseurs : fournisseursCrud.rows}
-      equipements={props.equipements?.length ? props.equipements : equipementsCrud.rows}
-    />
+    <div className="space-y-6">
+      <ImpactFarmValueBridge {...mergedProps} />
+      <ImpactBusinessStrategicV5 {...mergedProps} />
+    </div>
   );
 }
