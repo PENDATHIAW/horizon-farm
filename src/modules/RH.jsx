3be22bf4411a7af55e67225504e68fe55b718811
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-10 22:18:14 +0000

    feat: show payroll in RH module

--- src/modules/RH.jsx
+++ src/modules/RH.jsx
@@ -1,10 +1,12 @@
 import RHEquipe from './RHEquipe.jsx';
+import RHPayrollPanel from './RHPayrollPanel.jsx';
 import RHTeamManagementBridge from './RHTeamManagementBridge.jsx';
 
 export default function RH(props) {
   return (
     <div className="space-y-6">
       <RHEquipe {...props} />
+      <RHPayrollPanel />
       <RHTeamManagementBridge />
     </div>
   );
