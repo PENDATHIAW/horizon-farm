7d9ea8e795321118355cefb87da9701be2cb0127
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-10 18:24:18 +0000

    feat: display and sync direct sales margins in sales module

--- src/modules/VentesV3.jsx
+++ src/modules/VentesV3.jsx
@@ -1,10 +1,24 @@
 import SalesEvolution from './SalesEvolution.jsx';
+import SalesMarginsBridge from './SalesMarginsBridge.jsx';
 import VentesV2 from './VentesV2.jsx';
 
 export default function VentesV3(props) {
   return (
     <div className="space-y-6">
       <VentesV2 {...props} />
+      <SalesMarginsBridge
+        rows={props.rows || []}
+        lots={props.lots || []}
+        animaux={props.animaux || []}
+        cultures={props.cultures || []}
+        stocks={props.stocks || []}
+        alimentationLogs={props.alimentationLogs || []}
+        productionLogs={props.productionLogs || []}
+        vaccins={props.vaccins || []}
+        businessEvents={props.businessEvents || []}
+        onUpdate={props.onUpdate}
+        onRefresh={props.onRefresh}
+      />
       <SalesEvolution
         rows={props.rows || []}
         payments={props.paymentsList || props.payments || []}
