a3971f9d66a02e86d980cf5086ca676349000872
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-10 16:19:03 +0000

    feat: add feeding cost planner to stock module

--- src/modules/StocksV4.jsx
+++ src/modules/StocksV4.jsx
@@ -1,10 +1,16 @@
 import StocksV3 from './StocksV3.jsx';
 import StockEvolution from './StockEvolution.jsx';
+import StockFeedingCostPlanner from './StockFeedingCostPlanner.jsx';
 
 export default function StocksV4(props) {
   return (
     <div className="space-y-6">
       <StocksV3 {...props} />
+      <StockFeedingCostPlanner
+        rows={props.rows || []}
+        animaux={props.animaux || []}
+        lots={props.lots || []}
+      />
       <StockEvolution
         rows={props.rows || []}
         alimentationLogs={props.alimentationLogs || []}
