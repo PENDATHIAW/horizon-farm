80af7da534f66fd2b548e11b756bce25d0d6d2d1
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-10 21:49:39 +0000

    feat: show consolidated finance truth

--- src/modules/FinancesV11.jsx
+++ src/modules/FinancesV11.jsx
@@ -1,9 +1,18 @@
 import FinancesV10 from './FinancesV10.jsx';
 import FinanceEvolution from './FinanceEvolution.jsx';
+import FinanceConsolidationPanel from './FinanceConsolidationPanel.jsx';
 
 export default function FinancesV11(props) {
   return (
     <div className="space-y-6">
+      <FinanceConsolidationPanel
+        rows={props.rows || []}
+        salesOrders={props.salesOrders || []}
+        payments={props.payments || []}
+        fournisseurs={props.fournisseurs || []}
+        stocks={props.stocks || []}
+        onNavigate={props.onNavigate}
+      />
       <FinancesV10 {...props} />
       <FinanceEvolution
         rows={props.rows || []}
