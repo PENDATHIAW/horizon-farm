12766d9cba784f421904f8cda802542ed479a041
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-10 03:41:39 +0000

    Show equipment quick action forms

--- src/modules/Equipements.jsx
+++ src/modules/Equipements.jsx
@@ -3,12 +3,26 @@ import GenericCrudModule from '../components/GenericCrudModule';
 import { MODULE_FORM_FIELDS } from '../utils/constants';
 import { fmtCurrency } from '../utils/format';
 import EquipementsMaintenanceBridge from './EquipementsMaintenanceBridge.jsx';
+import EquipementsQuickActionsBridge from './EquipementsQuickActionsBridge.jsx';
 
 export default function Equipements(props) {
   const rows = props.rows || [];
   const fuel = rows.reduce((sum, row) => sum + Number(row.fuel_cost || 0), 0);
   return (
     <div className="space-y-6">
+      <EquipementsQuickActionsBridge
+        rows={rows}
+        onUpdate={props.onUpdate}
+        onRefresh={props.onRefresh}
+        onCreateTask={props.onCreateTask}
+        onRefreshTasks={props.onRefreshTasks}
+        onCreateAlert={props.onCreateAlert}
+        onRefreshAlertes={props.onRefreshAlertes}
+        onCreateFinanceTransaction={props.onCreateFinanceTransaction}
+        onRefreshFinances={props.onRefreshFinances}
+        onCreateBusinessEvent={props.onCreateBusinessEvent}
+        onRefreshBusinessEvents={props.onRefreshBusinessEvents}
+      />
       <EquipementsMaintenanceBridge
         rows={rows}
         tasks={props.tasks || []}
@@ -33,7 +47,7 @@ export default function Equipements(props) {
         sub="Machines - incubateurs - pompes - groupes - vehicules - maintenance"
         fields={MODULE_FORM_FIELDS.equipements}
         columns={['id', 'name', 'type', 'status', 'purchase_cost', 'maintenance_due', 'fuel_cost']}
-        initialValues={{ status: 'operationnel', type: 'machine' }}
+        initialValues={{ status: 'operationnel', type: 'machine', purchase_cost: 0, maintenance_cost: 0, fuel_cost: 0 }}
         addLabel="Ajouter equipement"
         exportTitle="Equipements Horizon Farm"
         kpis={[
