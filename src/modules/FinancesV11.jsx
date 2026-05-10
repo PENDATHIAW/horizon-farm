258bbaa2581faa0be2c80f668be0512e123273fa
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-10 04:08:58 +0000

    feat: add finances module wrapper with evolution section

---
+++ src/modules/FinancesV11.jsx
@@ -0,0 +1,11 @@
+import FinancesV10 from './FinancesV10.jsx';
+import FinanceEvolution from './FinanceEvolution.jsx';
+
+export default function FinancesV11(props) {
+  return (
+    <div className="space-y-6">
+      <FinancesV10 {...props} />
+      <FinanceEvolution rows={props.rows || []} salesOrders={props.salesOrders || []} payments={props.payments || []} />
+    </div>
+  );
+}
