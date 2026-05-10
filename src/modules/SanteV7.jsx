760fc0eb0a3dde433fd1d804e6f996f3fc11e53d
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-10 23:27:45 +0000

    fix: simplify health module layout

--- src/modules/SanteV7.jsx
+++ src/modules/SanteV7.jsx
@@ -1,14 +1,5 @@
 import SanteV6 from './SanteV6.jsx';
-import SanteEvolution from './SanteEvolution.jsx';
 
 export default function SanteV7(props) {
-  return (
-    <div className="space-y-6">
-      <SanteV6 {...props} />
-      <SanteEvolution
-        rows={props.rows || []}
-        onNavigate={props.onNavigate}
-      />
-    </div>
-  );
+  return <SanteV6 {...props} />;
 }
