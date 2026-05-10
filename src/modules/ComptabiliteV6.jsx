5b9786493e56d9b052ab3d4b917bab0773acdf37
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-10 23:27:12 +0000

    fix: simplify accounting module layout

--- src/modules/ComptabiliteV6.jsx
+++ src/modules/ComptabiliteV6.jsx
@@ -1,27 +1,5 @@
 import ComptabiliteV5 from './ComptabiliteV5.jsx';
-import ComptabiliteEvolution from './ComptabiliteEvolution.jsx';
-import FinanceConsolidationPanel from './FinanceConsolidationPanel.jsx';
 
 export default function ComptabiliteV6(props) {
-  const rows = props.transactions || props.finances || [];
-  return (
-    <div className="space-y-6">
-      <FinanceConsolidationPanel
-        rows={rows}
-        salesOrders={props.salesOrders || []}
-        payments={props.payments || []}
-        fournisseurs={props.fournisseurs || []}
-        stocks={props.stocks || []}
-        onNavigate={props.onNavigate}
-      />
-      <ComptabiliteV5 {...props} />
-      <ComptabiliteEvolution
-        transactions={props.transactions || []}
-        finances={props.finances || []}
-        salesOrders={props.salesOrders || []}
-        payments={props.payments || []}
-        onNavigate={props.onNavigate}
-      />
-    </div>
-  );
+  return <ComptabiliteV5 {...props} />;
 }
