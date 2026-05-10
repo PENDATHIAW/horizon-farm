389ccf4aed6c500b17d2e878d8aebe6f844d0c86
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-10 23:25:36 +0000

    fix: simplify finance module layout

--- src/modules/FinancesV11.jsx
+++ src/modules/FinancesV11.jsx
@@ -1,25 +1,5 @@
 import FinancesV10 from './FinancesV10.jsx';
-import FinanceEvolution from './FinanceEvolution.jsx';
-import FinanceConsolidationPanel from './FinanceConsolidationPanel.jsx';
 
 export default function FinancesV11(props) {
-  return (
-    <div className="space-y-6">
-      <FinanceConsolidationPanel
-        rows={props.rows || []}
-        salesOrders={props.salesOrders || []}
-        payments={props.payments || []}
-        fournisseurs={props.fournisseurs || []}
-        stocks={props.stocks || []}
-        onNavigate={props.onNavigate}
-      />
-      <FinancesV10 {...props} />
-      <FinanceEvolution
-        rows={props.rows || []}
-        salesOrders={props.salesOrders || []}
-        payments={props.payments || []}
-        onNavigate={props.onNavigate}
-      />
-    </div>
-  );
+  return <FinancesV10 {...props} />;
 }
