5ae99269fc85eb60c204e319edca3dbeca112f05
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-10 21:50:27 +0000

    feat: add consolidated finance view to accounting

--- src/modules/ComptabiliteV6.jsx
+++ src/modules/ComptabiliteV6.jsx
@@ -1,9 +1,19 @@
 import ComptabiliteV5 from './ComptabiliteV5.jsx';
 import ComptabiliteEvolution from './ComptabiliteEvolution.jsx';
+import FinanceConsolidationPanel from './FinanceConsolidationPanel.jsx';
 
 export default function ComptabiliteV6(props) {
+  const rows = props.transactions || props.finances || [];
   return (
     <div className="space-y-6">
+      <FinanceConsolidationPanel
+        rows={rows}
+        salesOrders={props.salesOrders || []}
+        payments={props.payments || []}
+        fournisseurs={props.fournisseurs || []}
+        stocks={props.stocks || []}
+        onNavigate={props.onNavigate}
+      />
       <ComptabiliteV5 {...props} />
       <ComptabiliteEvolution
         transactions={props.transactions || []}
