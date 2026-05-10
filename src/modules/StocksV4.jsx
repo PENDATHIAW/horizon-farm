037492c2788d62daff8785154cc9f41475181808
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-10 23:01:00 +0000

    feat: expose stock loss finance bridge

--- src/modules/StocksV4.jsx
+++ src/modules/StocksV4.jsx
@@ -1,11 +1,21 @@
 import StocksV3 from './StocksV3.jsx';
 import StockEvolution from './StockEvolution.jsx';
 import StockFeedingCostPlanner from './StockFeedingCostPlanner.jsx';
+import StockLossFinanceBridge from './StockLossFinanceBridge.jsx';
 
 export default function StocksV4(props) {
   return (
     <div className="space-y-6">
       <StocksV3 {...props} />
+      <StockLossFinanceBridge
+        rows={props.rows || []}
+        onUpdate={props.onUpdate}
+        onRefresh={props.onRefresh}
+        onCreateFinanceTransaction={props.onCreateFinanceTransaction}
+        onRefreshFinances={props.onRefreshFinances}
+        onCreateBusinessEvent={props.onCreateBusinessEvent}
+        onRefreshBusinessEvents={props.onRefreshBusinessEvents}
+      />
       <StockFeedingCostPlanner
         rows={props.rows || []}
         animaux={props.animaux || []}
