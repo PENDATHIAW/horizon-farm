cbb1790b4bf5eb28ff1dfa7e531f0a0418d04d68
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-10 15:56:16 +0000

    feat: include animal slaughter stock bridge

--- src/modules/AnimauxV2.jsx
+++ src/modules/AnimauxV2.jsx
@@ -1,10 +1,19 @@
 import Animaux from './Animaux.jsx';
+import AnimalSlaughterStockBridge from './AnimalSlaughterStockBridge.jsx';
 import AnimauxEvolution from './AnimauxEvolution.jsx';
 
 export default function AnimauxV2(props) {
   return (
     <div className="space-y-6">
       <Animaux {...props} />
+      <AnimalSlaughterStockBridge
+        rows={props.rows || []}
+        businessEvents={props.businessEvents || []}
+        onUpdate={props.onUpdate}
+        onRefresh={props.onRefresh}
+        onCreateBusinessEvent={props.onCreateBusinessEvent}
+        onRefreshBusinessEvents={props.onRefreshBusinessEvents}
+      />
       <AnimauxEvolution
         rows={props.rows || []}
         opportunities={props.opportunities || []}
