5aec9bc8a0704f0aac3ce0795d9d5847b897a179
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-11 23:11:18 +0000

    fix: provide fallback animal and lot updates in health module

--- src/modules/SanteV7.jsx
+++ src/modules/SanteV7.jsx
@@ -1,8 +1,16 @@
+import useCrudModule from '../hooks/useCrudModule';
 import HealthQualityControl from './HealthQualityControl.jsx';
 import SanteV6 from './SanteV6.jsx';
 import SanteEvolution from './SanteEvolution.jsx';
 
 export default function SanteV7(props) {
+  const animauxCrud = useCrudModule('animaux');
+  const avicoleCrud = useCrudModule('avicole');
+  const onUpdateAnimal = props.onUpdateAnimal || animauxCrud.update;
+  const onUpdateLot = props.onUpdateLot || avicoleCrud.update;
+  const onRefreshAnimals = props.onRefreshAnimals || animauxCrud.refresh;
+  const onRefreshLots = props.onRefreshLots || avicoleCrud.refresh;
+
   return (
     <div className="space-y-6">
       <SanteV6 {...props} />
@@ -10,14 +18,14 @@ export default function SanteV7(props) {
         rows={props.rows || []}
         stocks={props.stocks || []}
         transactions={props.transactions || []}
-        animaux={props.animaux || []}
-        lots={props.lots || []}
+        animaux={props.animaux || animauxCrud.rows || []}
+        lots={props.lots || avicoleCrud.rows || []}
         onUpdate={props.onUpdate}
-        onUpdateAnimal={props.onUpdateAnimal}
-        onUpdateLot={props.onUpdateLot}
+        onUpdateAnimal={onUpdateAnimal}
+        onUpdateLot={onUpdateLot}
         onRefresh={props.onRefresh}
-        onRefreshAnimals={props.onRefreshAnimals}
-        onRefreshLots={props.onRefreshLots}
+        onRefreshAnimals={onRefreshAnimals}
+        onRefreshLots={onRefreshLots}
       />
       <SanteEvolution
         rows={props.rows || []}
