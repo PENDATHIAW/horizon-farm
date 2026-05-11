7ab74149cd8ebee1ea83e60f729596aafed0e651
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-11 00:03:37 +0000

    fix: keep accounting evolution charts

--- src/modules/ComptabiliteV6.jsx
+++ src/modules/ComptabiliteV6.jsx
@@ -1,5 +1,17 @@
 import ComptabiliteV5 from './ComptabiliteV5.jsx';
+import ComptabiliteEvolution from './ComptabiliteEvolution.jsx';
 
 export default function ComptabiliteV6(props) {
-  return <ComptabiliteV5 {...props} />;
+  return (
+    <div className="space-y-6">
+      <ComptabiliteV5 {...props} />
+      <ComptabiliteEvolution
+        transactions={props.transactions || []}
+        finances={props.finances || []}
+        salesOrders={props.salesOrders || []}
+        payments={props.payments || []}
+        onNavigate={props.onNavigate}
+      />
+    </div>
+  );
 }
