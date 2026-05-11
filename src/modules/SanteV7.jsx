e5809b064d89bd8bc1460075e4f63849e705f81a
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-11 23:05:20 +0000

    feat: show health quality control in health module

--- src/modules/SanteV7.jsx
+++ src/modules/SanteV7.jsx
@@ -1,3 +1,4 @@
+import HealthQualityControl from './HealthQualityControl.jsx';
 import SanteV6 from './SanteV6.jsx';
 import SanteEvolution from './SanteEvolution.jsx';
 
@@ -5,6 +6,19 @@ export default function SanteV7(props) {
   return (
     <div className="space-y-6">
       <SanteV6 {...props} />
+      <HealthQualityControl
+        rows={props.rows || []}
+        stocks={props.stocks || []}
+        transactions={props.transactions || []}
+        animaux={props.animaux || []}
+        lots={props.lots || []}
+        onUpdate={props.onUpdate}
+        onUpdateAnimal={props.onUpdateAnimal}
+        onUpdateLot={props.onUpdateLot}
+        onRefresh={props.onRefresh}
+        onRefreshAnimals={props.onRefreshAnimals}
+        onRefreshLots={props.onRefreshLots}
+      />
       <SanteEvolution
         rows={props.rows || []}
         onNavigate={props.onNavigate}
