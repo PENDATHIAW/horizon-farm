ab143b0d644416560d232e7c27dd5bcaa0851794
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-12 07:34:59 +0000

    fix: avoid JSX in Clients js wrapper

--- src/modules/Clients.js
+++ src/modules/Clients.js
@@ -1,14 +1,17 @@
+import React from 'react';
 import ClientsBase from './Clients.jsx';
 import ClientQualityControl from './ClientQualityControl.jsx';
 
 export default function Clients(props) {
-  return <div className="space-y-6">
-    <ClientsBase {...props} />
-    <ClientQualityControl
-      rows={props.rows || []}
-      salesOrders={props.salesOrders || []}
-      payments={props.payments || props.paymentsList || []}
-      transactions={props.transactions || []}
-    />
-  </div>;
+  return React.createElement(
+    'div',
+    { className: 'space-y-6' },
+    React.createElement(ClientsBase, props),
+    React.createElement(ClientQualityControl, {
+      rows: props.rows || [],
+      salesOrders: props.salesOrders || [],
+      payments: props.payments || props.paymentsList || [],
+      transactions: props.transactions || [],
+    })
+  );
 }
