190bb2c708777576df9f4cfc9e7f4fdcfc7e4ab7
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-17 15:20:10 +0000

    Add audit run and correction lots section to Assistant ERP

--- src/modules/AssistantERPV2.jsx
+++ src/modules/AssistantERPV2.jsx
@@ -1,11 +1,13 @@
 import AssistantERP from './AssistantERP.jsx';
 import AssistantERPInsights from './AssistantERPInsights.jsx';
+import AuditRunAndCorrectionPanel from './AuditRunAndCorrectionPanel.jsx';
 import ErpAuditPanel from './ErpAuditPanel.jsx';
 
 export default function AssistantERPV2(props) {
   return (
     <div className="space-y-6">
       <AssistantERPInsights {...props} />
+      <AuditRunAndCorrectionPanel />
       <ErpAuditPanel />
       <AssistantERP {...props} />
     </div>
