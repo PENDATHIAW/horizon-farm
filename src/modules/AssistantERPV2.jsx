9acc965afc50ee9c62b0435bb9452af7924a25d5
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-17 16:23:01 +0000

    Add ERP agent connector to Assistant ERP

--- src/modules/AssistantERPV2.jsx
+++ src/modules/AssistantERPV2.jsx
@@ -1,12 +1,14 @@
 import AssistantERP from './AssistantERP.jsx';
 import AssistantERPInsights from './AssistantERPInsights.jsx';
 import AuditRunAndCorrectionPanel from './AuditRunAndCorrectionPanel.jsx';
+import ErpAgentConnectorPanel from './ErpAgentConnectorPanel.jsx';
 import ErpAuditPanel from './ErpAuditPanel.jsx';
 
 export default function AssistantERPV2(props) {
   return (
     <div className="space-y-6">
       <AssistantERPInsights {...props} />
+      <ErpAgentConnectorPanel />
       <AuditRunAndCorrectionPanel />
       <ErpAuditPanel />
       <AssistantERP {...props} />
