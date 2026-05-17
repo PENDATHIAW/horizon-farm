888fe57cb9e56d67a15332f1dab218f2f8b23767
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-17 18:56:28 +0000

    Show correction deployment status in Assistant ERP

--- src/modules/AssistantERPV2.jsx
+++ src/modules/AssistantERPV2.jsx
@@ -3,6 +3,7 @@ import AssistantERPInsights from './AssistantERPInsights.jsx';
 import AuditCoverageMatrixPanel from './AuditCoverageMatrixPanel.jsx';
 import AuditPackageLauncherPanel from './AuditPackageLauncherPanel.jsx';
 import AuditRunAndCorrectionPanel from './AuditRunAndCorrectionPanel.jsx';
+import CorrectionDeploymentStatusPanel from './CorrectionDeploymentStatusPanel.jsx';
 import ErpAgentConnectorPanel from './ErpAgentConnectorPanel.jsx';
 import ErpAuditPanel from './ErpAuditPanel.jsx';
 import HumanUiAuditPanel from './HumanUiAuditPanel.jsx';
@@ -12,6 +13,7 @@ export default function AssistantERPV2(props) {
   return (
     <div className="space-y-6">
       <AssistantERPInsights {...props} />
+      <CorrectionDeploymentStatusPanel />
       <ErpAgentConnectorPanel />
       <AuditPackageLauncherPanel />
       <AuditRunAndCorrectionPanel />
