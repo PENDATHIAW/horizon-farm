f2050b091362d6a1d2c12d9be43b463e3818d6b5
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-17 15:12:38 +0000

    Move ERP audit panel into Assistant ERP

--- src/modules/AssistantERPV2.jsx
+++ src/modules/AssistantERPV2.jsx
@@ -1,10 +1,12 @@
 import AssistantERP from './AssistantERP.jsx';
 import AssistantERPInsights from './AssistantERPInsights.jsx';
+import ErpAuditPanel from './ErpAuditPanel.jsx';
 
 export default function AssistantERPV2(props) {
   return (
     <div className="space-y-6">
       <AssistantERPInsights {...props} />
+      <ErpAuditPanel />
       <AssistantERP {...props} />
     </div>
   );
