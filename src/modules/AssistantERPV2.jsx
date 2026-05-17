88770f9c20f7b45a26bec2073849b3ec5f156bce
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-17 20:54:40 +0000

    Simplify Assistant ERP to GitHub audit roadmap flow

--- src/modules/AssistantERPV2.jsx
+++ src/modules/AssistantERPV2.jsx
@@ -1,11 +1,8 @@
 import AssistantERP from './AssistantERP.jsx';
 import AssistantERPInsights from './AssistantERPInsights.jsx';
 import AuditCoverageMatrixPanel from './AuditCoverageMatrixPanel.jsx';
-import AuditPackageLauncherPanel from './AuditPackageLauncherPanel.jsx';
-import AuditRunAndCorrectionPanel from './AuditRunAndCorrectionPanel.jsx';
-import CorrectionDeploymentStatusPanel from './CorrectionDeploymentStatusPanel.jsx';
-import ErpAgentConnectorPanel from './ErpAgentConnectorPanel.jsx';
 import ErpAuditPanel from './ErpAuditPanel.jsx';
+import GitHubAuditRoadmapPanel from './GitHubAuditRoadmapPanel.jsx';
 import HumanUiAuditPanel from './HumanUiAuditPanel.jsx';
 import ModuleAuditChecklistPanel from './ModuleAuditChecklistPanel.jsx';
 
@@ -13,14 +10,11 @@ export default function AssistantERPV2(props) {
   return (
     <div className="space-y-6">
       <AssistantERPInsights {...props} />
-      <CorrectionDeploymentStatusPanel />
-      <ErpAgentConnectorPanel />
-      <AuditPackageLauncherPanel />
-      <AuditRunAndCorrectionPanel />
+      <GitHubAuditRoadmapPanel />
+      <ErpAuditPanel />
       <AuditCoverageMatrixPanel dataMap={props?.dataMap || {}} />
       <HumanUiAuditPanel />
       <ModuleAuditChecklistPanel />
-      <ErpAuditPanel />
       <AssistantERP {...props} />
     </div>
   );
