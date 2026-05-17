c9c4e3619d8b23264578cb49625ff37a12776b64
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-17 18:07:00 +0000

    Add all-lot secure package launcher to Assistant ERP

--- src/modules/AssistantERPV2.jsx
+++ src/modules/AssistantERPV2.jsx
@@ -1,5 +1,6 @@
 import AssistantERP from './AssistantERP.jsx';
 import AssistantERPInsights from './AssistantERPInsights.jsx';
+import AuditPackageLauncherPanel from './AuditPackageLauncherPanel.jsx';
 import AuditRunAndCorrectionPanel from './AuditRunAndCorrectionPanel.jsx';
 import ErpAgentConnectorPanel from './ErpAgentConnectorPanel.jsx';
 import ErpAuditPanel from './ErpAuditPanel.jsx';
@@ -9,6 +10,7 @@ export default function AssistantERPV2(props) {
     <div className="space-y-6">
       <AssistantERPInsights {...props} />
       <ErpAgentConnectorPanel />
+      <AuditPackageLauncherPanel />
       <AuditRunAndCorrectionPanel />
       <ErpAuditPanel />
       <AssistantERP {...props} />
