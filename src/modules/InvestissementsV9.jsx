d5bd27ad032a39ba0932b38d9061d59f0a44d995
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-21 22:00:31 +0000

    Show forecast before investments

--- src/modules/InvestissementsV9.jsx
+++ src/modules/InvestissementsV9.jsx
@@ -1,5 +1,6 @@
+import FinancialPlanPanel from './FinancialPlanPanel.jsx';
 import InvestissementsV8 from './InvestissementsV8.jsx';
 
 export default function InvestissementsV9(props) {
-  return <InvestissementsV8 {...props} />;
+  return <div className="space-y-6"><FinancialPlanPanel {...props} /><InvestissementsV8 {...props} /></div>;
 }
