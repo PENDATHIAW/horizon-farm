a821089c071c2c867465629ad0012c4559584ddf
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-24 23:24:50 +0000

    feat: afficher questions rapides assistant erp

--- src/modules/AssistantERPV2.jsx
+++ src/modules/AssistantERPV2.jsx
@@ -1,10 +1,12 @@
 import AssistantERP from './AssistantERP.jsx';
 import AssistantERPInsights from './AssistantERPInsights.jsx';
+import AssistantERPQuickAnswers from './AssistantERPQuickAnswers.jsx';
 
 export default function AssistantERPV2(props) {
   return (
     <div className="space-y-6">
       <AssistantERPInsights {...props} />
+      <AssistantERPQuickAnswers {...props} />
       <AssistantERP {...props} />
     </div>
   );
