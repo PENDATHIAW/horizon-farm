20cb68dab8962ede1bcac1dcfe455638689aef9d
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-24 14:37:43 +0000

    fix: retirer bloc ressources internes des equipements

--- src/modules/EquipementsV2.jsx
+++ src/modules/EquipementsV2.jsx
@@ -1,15 +1,9 @@
-import InternalResourcesHealth from './InternalResourcesHealth.jsx';
 import Equipements from './Equipements.jsx';
 
 export default function EquipementsV2(props) {
-  return <div className="space-y-6">
-    <InternalResourcesHealth
-      equipements={props.rows || []}
-      transactions={props.transactions || []}
-      documents={props.documents || []}
-      tasks={props.tasks || []}
-      onNavigate={props.onNavigate}
-    />
-    <Equipements {...props} />
-  </div>;
+  return (
+    <div className="space-y-6">
+      <Equipements {...props} />
+    </div>
+  );
 }
