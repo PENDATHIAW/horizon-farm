a40f02a4225882237acc637e9184fc0d458d3692
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-24 13:24:40 +0000

    feat: afficher controle KPI BP dans rapports

--- src/modules/RapportsV2.jsx
+++ src/modules/RapportsV2.jsx
@@ -1,9 +1,17 @@
+import BpKpiHealth from './BpKpiHealth.jsx';
 import FinanceAccountingHealth from './FinanceAccountingHealth.jsx';
 import Rapports from './Rapports.jsx';
 
 export default function RapportsV2(props) {
   const data = props.data || {};
   return <div className="space-y-6">
+    <BpKpiHealth
+      salesOrders={data.salesOrders || data.sales_orders || []}
+      payments={data.payments || []}
+      transactions={data.transactions || data.finances || []}
+      investments={data.investissements || []}
+      onNavigate={props.onNavigate}
+    />
     <FinanceAccountingHealth
       transactions={data.transactions || data.finances || []}
       salesOrders={data.salesOrders || data.sales_orders || []}
