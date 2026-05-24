67a3248f6c6c2dc2b38acda3af80ea3250c4a252
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-24 12:37:51 +0000

    feat: ajouter controle finance compta aux rapports

---
+++ src/modules/RapportsV2.jsx
@@ -0,0 +1,18 @@
+import FinanceAccountingHealth from './FinanceAccountingHealth.jsx';
+import Rapports from './Rapports.jsx';
+
+export default function RapportsV2(props) {
+  const data = props.data || {};
+  return <div className="space-y-6">
+    <FinanceAccountingHealth
+      transactions={data.transactions || data.finances || []}
+      salesOrders={data.salesOrders || data.sales_orders || []}
+      payments={data.payments || []}
+      documents={data.documents || []}
+      clients={data.clients || []}
+      fournisseurs={data.fournisseurs || []}
+      onNavigate={props.onNavigate}
+    />
+    <Rapports {...props} />
+  </div>;
+}
