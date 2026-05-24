f77f28e95efb4cfdee398b908b6162468a42fed2
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-24 19:25:50 +0000

    feat: afficher comptabilite automatique en premier

--- src/modules/ComptabiliteV7.jsx
+++ src/modules/ComptabiliteV7.jsx
@@ -1,15 +1,22 @@
 import useCrudModule from '../hooks/useCrudModule';
+import AutomaticAccountingPanel from './AutomaticAccountingPanel.jsx';
 import FinanceAccountingHealth from './FinanceAccountingHealth.jsx';
 import ComptabiliteV6 from './ComptabiliteV6.jsx';
 
 export default function ComptabiliteV7(props) {
   const documentsCrud = useCrudModule('documents');
+  const documents = props.documents || documentsCrud.rows || [];
   return <div className="space-y-6">
+    <AutomaticAccountingPanel
+      transactions={props.transactions || props.finances || []}
+      documents={documents}
+      onNavigate={props.onNavigate}
+    />
     <FinanceAccountingHealth
       transactions={props.transactions || props.finances || []}
       salesOrders={props.salesOrders || []}
       payments={props.payments || []}
-      documents={props.documents || documentsCrud.rows || []}
+      documents={documents}
       clients={props.clients || []}
       fournisseurs={props.fournisseurs || []}
       onNavigate={props.onNavigate}
