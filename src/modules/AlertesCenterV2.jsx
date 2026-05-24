17df7d3d0de6a73295e791b9a32dbe943ca747d2
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-24 19:18:20 +0000

    feat: afficher routines ferme dans alertes

--- src/modules/AlertesCenterV2.jsx
+++ src/modules/AlertesCenterV2.jsx
@@ -1,8 +1,15 @@
 import ActionTraceHealth from './ActionTraceHealth.jsx';
 import AlertesCenterTechnical from './AlertesCenterTechnical.jsx';
+import FarmRoutineTasksPanel from './FarmRoutineTasksPanel.jsx';
 
 export default function AlertesCenterV2(props) {
   return <div className="space-y-6">
+    <FarmRoutineTasksPanel
+      tasks={props.tasks || []}
+      onCreateTask={props.onCreateTask}
+      onRefreshTasks={props.onRefreshTasks}
+      onNavigate={props.onNavigate}
+    />
     <ActionTraceHealth
       tasks={props.tasks || []}
       alertes={props.alertes || []}
