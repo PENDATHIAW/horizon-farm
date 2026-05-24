6cdcb12651012c9c6f50f8c48078cff2eb0ee7df
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-24 19:17:49 +0000

    feat: afficher routines ferme dans taches

--- src/modules/TachesV3.jsx
+++ src/modules/TachesV3.jsx
@@ -1,8 +1,15 @@
 import ActionTraceHealth from './ActionTraceHealth.jsx';
+import FarmRoutineTasksPanel from './FarmRoutineTasksPanel.jsx';
 import TachesTechnical from './TachesTechnical.jsx';
 
 export default function TachesV3(props) {
   return <div className="space-y-6">
+    <FarmRoutineTasksPanel
+      tasks={props.rows || []}
+      onCreateTask={props.onCreate}
+      onRefreshTasks={props.onRefresh}
+      onNavigate={props.onNavigate}
+    />
     <ActionTraceHealth
       tasks={props.rows || []}
       alertes={props.alertes || []}
