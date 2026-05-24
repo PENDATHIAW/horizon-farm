93da7d1abc7d659faeeb6edbd91132716fd550e5
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-24 13:23:53 +0000

    feat: ajouter controle KPI aux objectifs

---
+++ src/modules/ObjectifsCroissanceV2.jsx
@@ -0,0 +1,16 @@
+import BpKpiHealth from './BpKpiHealth.jsx';
+import ObjectifsCroissance from './ObjectifsCroissance.jsx';
+
+export default function ObjectifsCroissanceV2(props) {
+  const dataMap = props.dataMap || {};
+  return <div className="space-y-6">
+    <BpKpiHealth
+      salesOrders={dataMap.salesOrders || dataMap.sales_orders || []}
+      payments={dataMap.payments || []}
+      transactions={dataMap.transactions || dataMap.finances || []}
+      investments={dataMap.investissements || []}
+      onNavigate={props.onNavigate}
+    />
+    <ObjectifsCroissance {...props} />
+  </div>;
+}
