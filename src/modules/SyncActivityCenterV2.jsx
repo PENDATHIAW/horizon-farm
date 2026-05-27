8e64654b80fa2a4d4ddaa6b20716354af0daeac8
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-27 23:53:29 +0000

    Route sync activity to redesigned module

--- src/modules/SyncActivityCenterV2.jsx
+++ src/modules/SyncActivityCenterV2.jsx
@@ -1,16 +1,1 @@
-import ActionTraceHealth from './ActionTraceHealth.jsx';
-import SyncActivityCenter from './SyncActivityCenter.jsx';
-
-export default function SyncActivityCenterV2(props) {
-  return <div className="space-y-6">
-    <ActionTraceHealth
-      tasks={props.tasks || []}
-      alertes={props.alertes || []}
-      events={props.businessEvents || []}
-      auditLogs={props.auditLogs || []}
-      online={props.online}
-      onNavigate={props.onNavigate}
-    />
-    <SyncActivityCenter {...props} />
-  </div>;
-}
+export { default } from './SyncERPModule';
