b27ae7c8c392e2115506aff8df33ed019e1b5653
PENDATHIAW <pendathiaw1995@gmail.com>
2026-05-27 23:26:13 +0000

    Route RH to operations resources module

--- src/modules/RHV2.jsx
+++ src/modules/RHV2.jsx
@@ -1,15 +1,1 @@
-import InternalResourcesHealth from './InternalResourcesHealth.jsx';
-import RH from './RH.jsx';
-
-export default function RHV2(props) {
-  return <div className="space-y-6">
-    <InternalResourcesHealth
-      equipements={props.equipements || []}
-      transactions={props.transactions || []}
-      documents={props.documents || []}
-      tasks={props.tasks || []}
-      onNavigate={props.onNavigate}
-    />
-    <RH {...props} />
-  </div>;
-}
+export { default } from './OperationsRessourcesModule';
